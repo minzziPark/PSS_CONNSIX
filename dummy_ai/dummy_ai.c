fe01354a45dd22ba00a8d2f7b192bb6c845e00e4
minzziPark <minzzi0068@gmail.com>
2023-12-12 20:51:09 +0900

    코드 수정

--- dummy_ai/dummy_ai.c
+++ dummy_ai/dummy_ai.c
@@ -6,9 +6,140 @@
 #include "../include/connsix.h"
 #include "../include/parsing.h"
 
-char board[19][19] = {'E'};
+#define BOARD_SIZE 19
+#define INFINITY 100000
 
+typedef struct {
+    int x;
+    int y;
+} Move;
+
+char board[BOARD_SIZE][BOARD_SIZE] = {'E'};
 char wbuf[10] ;	
+int ai_color = 0; // 0->black, 1->white
+int direction_x[4] = {1, 0, 1, 1};
+int direction_y[4] = {0, 1, -1, 1};
+
+char set_color(int which_color) {
+  return which_color == ai_color ? 'B' : 'W';
+}
+
+void print_board() {
+  for(int i=0; i<BOARD_SIZE; i++){
+		for(int j=0; j<BOARD_SIZE; j++){
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int evaluate_4_way(int x, int y, char eval_color) {
+	int score = 0;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 6; j++) {
+			int nx = x + j * direction_x[i];
+			int ny = y + j * direction_y[i];
+
+			if (nx < 0 || nx >= BOARD_SIZE || ny < 0 || ny >= BOARD_SIZE) {
+				break;
+			} else if (board[nx][ny] != eval_color) {
+				break;
+			}
+			score++;
+		}
+	}
+	return score;
+}
+
+int evaluate_board() {
+	int score = 0;
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (board[i][j] == 'B') {
+				score += evaluate_4_way(i, j, 'B');
+			} else if (board[i][j] == 'W') {
+				score -= evaluate_4_way(i, j, 'W');
+			}
+		}
+	}
+	return score;
+}
+
+int is_valid_move(int x, int y) {
+	return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE && board[x][y] == 'E';
+}
+
+int heuristic(int depth) {
+	// 휴리스틱을 사용하여 게임 상태를 빠르게 평가
+	int score = evaluate_board();
+	
+	// 휴리스틱에 따라 평가 점수에 가중치 부여
+	return score + depth;
+}
+
+int minimax(int depth, int my_turn, int alpha, int beta) {
+	if (depth == 0) {
+		return heuristic(depth);
+	}
+
+	int best_score = my_turn ? -INFINITY : INFINITY;
+
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (is_valid_move(i, j)) {
+				board[i][j] = set_color(my_turn);
+				int candidate_score = minimax(depth - 1, !my_turn, alpha, beta);
+				board[i][j] = 'E';
+
+				if (my_turn) {
+					if (candidate_score > best_score) {
+						best_score = candidate_score;
+						alpha = candidate_score;
+					}
+				} else {
+					if (candidate_score < best_score) {
+						best_score = candidate_score;
+						beta = candidate_score;
+					}
+				}
+
+				if (alpha >= beta) {
+					// 가지치기
+					return best_score;
+				}
+			}
+		}
+	}
+
+	return best_score;
+}
+
+Move find_best_position() {
+	int best_score = -INFINITY;
+	Move best_position = {-1, -1};
+
+	int alpha = -INFINITY;
+	int beta = INFINITY;
+
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (is_valid_move(i, j)) {
+				board[i][j] = set_color(ai_color);
+				int candidate_score = minimax(2, 0, alpha, beta); // Adjust depth as needed
+				board[i][j] = 'E';
+
+				if (candidate_score > best_score) {
+					best_score = candidate_score;
+					best_position.x = i;
+					best_position.y = j;
+				}
+			}
+		}
+	}
+
+	return best_position;
+}
+
 
 int
 main ()
@@ -45,6 +176,7 @@ main ()
 		parsing_and_draw(board, "K10", color);
 	}
 	else{
+		ai_color = 1;
 		strcpy(opp_color, "black");
 		first = draw_and_read("") ;
 	}
@@ -56,26 +188,16 @@ main ()
 	printf("Read %s from server.\n", first) ;
 	parsing_and_draw(board, first, opp_color);
 
-	char hor1 = '\0' ;
-	char hor2 = '\0' ;
-	int ver1 = 0 ;
-	int ver2 = 0 ;
-	srand(time(0x0)) ;
-
 	while (1) {
-		for(int i=0; i<19; i++){
-			for(int j=0; j<19; j++){
-				printf("%c ", board[i][j]);
-			}
-			printf("\n");
-		}
-		hor1 = (rand() % 19) + 'A' ;
-		ver1 = (rand() % 19) + 1 ;
-			
-		hor2 = (rand() % 19) + 'A' ;
-		ver2 = (rand() % 19) + 1 ;
+		print_board();
+
+		Move best_move1 = find_best_position();
+		printf("Best Move: %c%d\n", 'A' + best_move1.y, best_move1.x + 1);
+
+		Move best_move2 = find_best_position();
+		printf("Best Move: %c%d\n", 'A' + best_move2.y, best_move2.x + 1);
 		
-		snprintf(wbuf, 10, "%c%02d:%c%02d", hor1, ver1, hor2, ver2) ;
+		snprintf(wbuf, 10, "%c%02d:%c%02d", best_move1.y, best_move1.x, best_move2.y, best_move2.x);
 		parsing_and_draw(board, wbuf, color);
 
 		char * rbuf = draw_and_read(wbuf) ;
